76c2841550f4444d8545b0f6b9570ca907f454bb
tq-bit <69204418+tq-bit@users.noreply.github.com>
2021-01-03 21:17:24 +0100

    Added quotes around the host

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const host = http://localhost
+const host = 'http://localhost';
 const port = 3000
 
 // We'll use the public directory to serve the Vue App
